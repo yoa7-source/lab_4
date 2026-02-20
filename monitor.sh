d613184c17930b4610faecca99748b72a6e5fb1a
Yousef <yoa7@njit.edu>
2026-02-20 13:41:48 -0500

    Update header on main branch

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Log thedate and memory usage
 
-echo "Memory Log - $(date)" >> system_log.txt
+echo "DAILY MEMORY CHECK - $(date)" >> system_log.txt
 free -h | grep Mem >> system_log.txt
 echo "--------------------------------" >> system_log.txt
